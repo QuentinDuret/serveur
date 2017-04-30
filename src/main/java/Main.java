58fc27da37a53cc03f975bf5d3dbd45725a6619a
dq610272 <quentin.duret@etu.unice.fr>
2017-04-30 22:29:21 +0200

    server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
-/**
- * Created by sebde on 30/04/2017.
- */
+package main.java;
+
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("bonjour");
+    public static void main(String[] args) {
+
+        String host = "127.0.0.1";
+        int port = 2345;
+
+        Server ts = new Server(host, port);
+        ts.open();
+
+        System.out.println("Serveur initialisé.");
+
     }
 }
